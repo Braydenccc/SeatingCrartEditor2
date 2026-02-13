4c3c6da79d5acfa7f4c68f2a1d18f367773a0630
jby <a278009397@163.com>
2026-02-13 20:35:01 +0800

    错字受

--- src/components/layout/AppHeader.vue
+++ src/components/layout/AppHeader.vue
@@ -1,7 +1,7 @@
 <template>
   <header class="app-header">
     <h1 class="header-text">BraydenSCE V2</h1>
-    <p class="header-subtitle">座位表编辑器 开发版本 <a href="https://afdian.com/a/brayden">byccc</a> 由<a href="https://host.retiehe.com/">热铁盒网络托管</a>提供服务 | <a href="https://github.com/Braydenccc/SeatingCrartEditor2" target="_blank">仓库链接</a></p>
+    <p class="header-subtitle">座位表编辑器 开发版本 <a href="https://afdian.com/a/brayden">byccc</a> 由<a href="https://host.retiehe.com/">热铁盒网页托管</a>提供服务 | <a href="https://github.com/Braydenccc/SeatingCrartEditor2" target="_blank">仓库链接</a></p>
   </header>
 </template>
 
